c6cfb0661cc0602a9607557db96d6bdeb3f2ceb1
KlaraPlchova4 <klaraplchova2@gmail.com>
2025-04-28 09:32:29 +0200

    přidání fiba

--- fibo.py
+++ fibo.py
@@ -1,5 +1,18 @@
+def fiboo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fiboo(n - 1) + fiboo(n - 2)
+print(fiboo(18))
 
 def main():
+    number = int(input("Zadej číslo:"))
+    vystup = []
+    for i in range(number + 1):
+        vystup.append(fiboo(i))
+    print(vystup)
     pass
 
 
